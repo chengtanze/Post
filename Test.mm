53f37936d6a7e8b2fd0d4b028f5fdd571a5a2ce9
chengtz <chengtanze@126.com>
2015-01-30 18:13:16 +0800

    百度地图

---
+++ Test.mm
@@ -0,0 +1,21 @@
+//
+//  Test.m
+//  Post
+//
+//  Created by wangsl-iMac on 15/1/30.
+//  Copyright (c) 2015年 cheng. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
