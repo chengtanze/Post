555a843624a17c1e12140941c2db93cc02c607ca
chengtanze <cheng@chengdeMacBook-Pro.local>
2015-03-03 22:50:05 +0800

    增加订单详情页面

---
+++ Post/OrderDetailTableViewController.h
@@ -0,0 +1,25 @@
+//
+//  OrderDetailTableViewController.h
+//  Post
+//
+//  Created by cheng on 15/3/3.
+//  Copyright (c) 2015年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderDetailTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UILabel *goodsName;
+@property (weak, nonatomic) IBOutlet UILabel *goodsType;
+@property (weak, nonatomic) IBOutlet UILabel *goodsVaule;
+@property (weak, nonatomic) IBOutlet UILabel *OrderNO;
+@property (weak, nonatomic) IBOutlet UILabel *orderState;
+@property (weak, nonatomic) IBOutlet UILabel *vehicleType;
+@property (weak, nonatomic) IBOutlet UILabel *goodsWeight;
+@property (weak, nonatomic) IBOutlet UIView *balanceLable;
+@property (weak, nonatomic) IBOutlet UIView *shipperText;
+@property (weak, nonatomic) IBOutlet UILabel *shipperPhoneNOText;
+@property (weak, nonatomic) IBOutlet UILabel *messrsLable;
+@property (weak, nonatomic) IBOutlet UILabel *messrsPhoneNOText;
+
+@end
