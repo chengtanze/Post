3f3630b7fc596da06cde8b234d0518c78a226264
chengtz <chengtanze@126.com>
2015-03-23 18:01:17 +0800

    添加 “常用地址”

---
+++ Post/CommonAddressController.h
@@ -0,0 +1,13 @@
+//
+//  CommonAddressController.h
+//  Post
+//
+//  Created by wangsl-iMac on 15/3/23.
+//  Copyright (c) 2015年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonAddressController : UITableViewController
+
+@end
